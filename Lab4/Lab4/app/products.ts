2eacf0385c7496b820854ebb8ffb6ca9b5612e24
Beibit Suleimen <54830786+Beibit123@users.noreply.github.com>
2021-02-24 10:42:03 +0600

    Add files via upload

---
+++ Lab4/Lab4/app/products.ts
@@ -0,0 +1,93 @@
+export const products = [
+    {
+      id: 1,
+      name: 'Marvel s Spider-Man: Miles Morales Launch Edition - PlayStation 4',
+      price: 49.99,
+      description: 'Video game Marvel s Spider-Man: Miles Morales for PlayStation 4',
+      imgurl: '../assets/img/spiderManMilesMorales.png',
+      url:'https://www.amazon.com/Marvels-Spider-Man-Miles-Morales-Launch-PlayStation/dp/B08JHVG9ZJ/ref=sr_1_3?dchild=1&fst=as%3Aoff&pf_rd_i=16225016011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=03b28c2c-71e9-4947-aa06-f8b5dc8bf880&pf_rd_r=8NHAJ4RRY97T659RWB5T&pf_rd_s=merchandised-search-3&pf_rd_t=101&qid=1510597020&refinements=p_89%3APlaystation&rnid=2528832011&s=videogames-intl-ship&sr=1-3',
+      rating: 4.9
+    },
+    {
+      id: 2,
+      name: 'Mortal Kombat 11: Aftermath Kollection - PlayStation 4',
+      price: 39.99,
+      description: 'Video game Mortal Kombat 11 for PlayStation 4',
+      imgurl: '../assets/img/mortalCombat.png',
+      url:'https://www.amazon.com/Mortal-Kombat-11-Aftermath-Kollection-PlayStation/dp/B08863N9WZ/ref=pd_sbs_14?pd_rd_w=qUX2K&pf_rd_p=3ec6a47e-bf65-49f8-80f7-0d7c7c7ce2ca&pf_rd_r=F80C023AV3RMPB13QBYY&pd_rd_r=8159298a-9347-4620-bfad-85ec8c2a7c7c&pd_rd_wg=9UbQU&pd_rd_i=B08863N9WZ&psc=1',
+      rating: 4.8
+    },
+    {
+      id: 3,
+      name: 'Assassin’s Creed Valhalla PlayStation 4 Standard Edition',
+      price: 39.99,
+      description: 'Video game Assassin’s Creed Valhalla for PlayStation 4',
+      imgurl: '../assets/img/Valhalla.png',
+      url:'https://www.amazon.com/Assassins-Valhalla-PlayStation-Standard-upgrade-4/dp/B087XTT5GZ/ref=pd_sbs_3?pd_rd_w=4LAmN&pf_rd_p=3ec6a47e-bf65-49f8-80f7-0d7c7c7ce2ca&pf_rd_r=W0F2QKK51PGMAWXKVC85&pd_rd_r=2039a387-f691-4a44-abe3-92cbe0d25f24&pd_rd_wg=HqDve&pd_rd_i=B087XTT5GZ&psc=1',
+      rating: 4.7
+    },
+    {
+      id: 4,
+      name: 'Call of Duty: Black Ops Cold War (PS4)',
+      price: 54.99,
+      description: 'Video game Call of Duty: Black Ops Cold War for PlayStation 4',
+      imgurl: '../assets/img/callOfDutyColdWar.png',
+      url:'https://www.amazon.com/Call-Duty-Black-Cold-PlayStation-4/dp/B08GR4QMC9/ref=pd_sbs_1?pd_rd_w=Tb9QD&pf_rd_p=3ec6a47e-bf65-49f8-80f7-0d7c7c7ce2ca&pf_rd_r=60ZJ7PXGQC1Q5PJQ087R&pd_rd_r=f3f71dfc-91cc-45aa-8ac6-aaebc9ad2ea5&pd_rd_wg=HGJ3D&pd_rd_i=B08GR4QMC9&psc=1',
+      rating: 4.6
+    },
+    {
+      id: 5,
+      name: 'Call of Duty: Modern Warfare (PS4)',
+      price: 75.00,
+      description: 'Video game Call of Duty: Modern Warfare for PlayStation 4',
+      imgurl: '../assets/img/callOfDutyModernWarfare.png',
+      url:'https://www.amazon.com/Call-Duty-Modern-Warfare-playstation-4/dp/B07ZNVGJS1/ref=pd_sbs_5?pd_rd_w=E263K&pf_rd_p=3ec6a47e-bf65-49f8-80f7-0d7c7c7ce2ca&pf_rd_r=0DXM72GZQWYTTSYC5GK0&pd_rd_r=d4a34916-3882-4143-89e8-347300630928&pd_rd_wg=gPs28&pd_rd_i=B07ZNVGJS1&psc=1',
+      rating: 4.4
+    },
+    {
+      id: 6,
+      name: 'Watch Dogs Legion - PlayStation 4 Standard Edition',
+      price: 29.99,
+      description: 'Video game Watch Dogs Legion for PlayStation 4',
+      imgurl: '../assets/img/watchDogsLegion.png',
+      url:'https://www.amazon.com/Watch-Dogs-Legion-PlayStation-4-Standard/dp/B07SH36GRX/ref=pd_sbs_12?pd_rd_w=4fKrK&pf_rd_p=3ec6a47e-bf65-49f8-80f7-0d7c7c7ce2ca&pf_rd_r=QG7WGC9P0TXVKGK9H76Q&pd_rd_r=2d5de0ec-44d3-4166-8f0b-4a26ee779858&pd_rd_wg=lg39G&pd_rd_i=B07SH36GRX&psc=1',
+      rating: 4.6
+    },
+    {
+      id: 7,
+      name: 'Cyberpunk 2077 - PlayStation 4',
+      price: 27.58,
+      description: 'Video game Cyberpunk 2077 for PlayStation 4',
+      imgurl: '../assets/img/cyberpunk.png',
+      url:'https://www.amazon.com/Cyberpunk-2077-PlayStation-4/dp/B07DJWBYKP/ref=pd_sbs_5?pd_rd_w=Myxod&pf_rd_p=3ec6a47e-bf65-49f8-80f7-0d7c7c7ce2ca&pf_rd_r=WW9BVSA81K7ATC21R5PA&pd_rd_r=2e67e010-c137-41f4-95c1-bbb2c90a656d&pd_rd_wg=nAaMO&pd_rd_i=B07DJWBYKP&psc=1',
+      rating: 3.7
+    },
+    {
+      id: 8,
+      name: 'Ghost of Tsushima - PlayStation 4',
+      price: 55.00,
+      description: 'Video game Ghost of Tsushima for PlayStation 4',
+      imgurl: '../assets/img/ghostOfThusima.png',
+      url:'https://www.amazon.com/Ghost-Tsushima-PlayStation-4/dp/B08BSKT43L/ref=pd_sbs_2?pd_rd_w=MPY7I&pf_rd_p=3ec6a47e-bf65-49f8-80f7-0d7c7c7ce2ca&pf_rd_r=VXDQKQKPYM7BKEDHX4PY&pd_rd_r=10b0253b-d41d-42cd-99b1-6e4470b5b274&pd_rd_wg=1fVwD&pd_rd_i=B08BSKT43L&psc=1',
+      rating: 4.9
+    },
+    {
+      id: 9,
+      name: 'The Last of Us Part II - PlayStation 4',
+      price: 29.83,
+      description: 'Video game The Last of Us Part II for PlayStation 4',
+      imgurl: '../assets/img/tlou2.png',
+      url:'https://www.amazon.com/Last-Us-Part-II-PlayStation-4/dp/B07DJRFSDF/ref=pd_sbs_1?pd_rd_w=irPLZ&pf_rd_p=3ec6a47e-bf65-49f8-80f7-0d7c7c7ce2ca&pf_rd_r=9K8C6BY6NKQP42402CX7&pd_rd_r=6781a463-a041-4990-a86f-d48fac9e72e1&pd_rd_wg=3INcT&pd_rd_i=B07DJRFSDF&psc=1',
+      rating: 4.7
+    },
+    {
+      id: 10,
+      name: 'Days Gone - Playstation 4',
+      price: 24.99,
+      description: 'Video game Days Gone for PlayStation 4',
+      imgurl: '../assets/img/daysGone.png',
+      url:'https://www.amazon.com/Days-Gone-Playstation-4/dp/B01GW8XRCG/ref=pd_sbs_4?pd_rd_w=pojJ5&pf_rd_p=3ec6a47e-bf65-49f8-80f7-0d7c7c7ce2ca&pf_rd_r=1Y1KRHQEKGKT1FW3VK99&pd_rd_r=8244abad-e475-4460-b948-abc76222fb54&pd_rd_wg=3MrTq&pd_rd_i=B01GW8XRCG&psc=1',
+      rating: 4.6
+    }
+  ];
+  
